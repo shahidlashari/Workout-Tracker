26a155febf08be7298d486245c309eba7464923a
shahid lashari <shahid_lashari001@yahoo.com>
2020-05-15 21:50:37 -0700

    Created Server.js having process.env.MONGODB_URI localhost database

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fitness_tracker_db', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+
+app.listen(PORT);
